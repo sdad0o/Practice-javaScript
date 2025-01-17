c1cac36f009c93fe3b8553850fb7b8ebfd9920f3
sdad0o <slwdsaw@gmail.com>
2025-01-17 23:31:56 +0300

    Refaactor the code and remove the dublicate code

--- Guess My Number(DOM)/script.js
+++ Guess My Number(DOM)/script.js
@@ -1,24 +1,24 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);//to read the text in the element
-// document.querySelector('.message').textContent ='Correct number 🎉';
-// document.querySelector('.number').textContent =13;
-// document.querySelector('.score').textContent =10;
-// document.querySelector('.guess').value=23;
-// console.log(document.querySelector('.guess').value);
 let secretNumber= Math.trunc(Math.random()*20)+1;
 let score =20;
 let highscore =0;
-// document.querySelector('.number').textContent =secretNumber;
+// Create the function to make the code cleaner and remove the dublicate code 
+const displayMessage = function(message)
+{
+    document.querySelector('.message').textContent=message;
+}
+
 document.querySelector('.check').addEventListener('click',()=>{
     // this function called event handeller -- announmes function 
     const guess = Number(document.querySelector('.guess').value);
     if (!guess)
     {
-        document.querySelector('.message').textContent='⛔ No number !';
+        // document.querySelector('.message').textContent='⛔ No number !';
+        displayMessage('⛔ No number !');
     }
     else if(guess === secretNumber)
     {
-       document.querySelector('.message').textContent='🎉 Correct number';
+       displayMessage('🎉 Correct number');
        document.querySelector('.number').textContent =secretNumber;
        document.querySelector('body').style.backgroundColor ='#60b347';
        document.querySelector('.number').style.width='30rem';
@@ -29,44 +29,29 @@ document.querySelector('.check').addEventListener('click',()=>{
        }
 
     }
-    else if (guess> secretNumber)
+    // Refaactor the code and remove the dublicate in this way 
+     else if (guess !== secretNumber)
     {
         if(score>1)
         {
-            document.querySelector('.message').textContent='📈 Too high';
+            displayMessage (guess>secretNumber ?'📈 Too high':'📉 Too low');
             score--;
             document.querySelector('.score').textContent =score;
         }
         else
         {
-            document.querySelector('.message').textContent='😑 You lost the game';
-            document.querySelector('.score').textContent =0;
-            document.querySelector('body').style.backgroundColor ='#ff1919';
-            document.querySelector('.number').textContent =secretNumber;
-        }
-        
-    }
-    else if (guess < secretNumber)
-    {
-        if(score>1)
-        {
-            document.querySelector('.message').textContent='📉 Too low';
-            score--;
-            document.querySelector('.score').textContent =score;
-        }
-        else
-        {
-            document.querySelector('.message').textContent='😑 You lost the game';
+            displayMessage('😑 You lost the game');
             document.querySelector('.score').textContent =0;
             document.querySelector('body').style.backgroundColor ='#ff1919';
             document.querySelector('.number').textContent =secretNumber;
         }
     }
+    
 });
 document.querySelector('.again').addEventListener('click',()=>{
     score =20;
     secretNumber= Math.trunc(Math.random()*20)+1;
-    document.querySelector('.message').textContent='Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent =score;
     document.querySelector('.number').textContent='?';
     document.querySelector('.guess').value='';
